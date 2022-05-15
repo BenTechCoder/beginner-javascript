d395c6bc2e41ad6f24bf7676f8fd37a5b9f9d7f0
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2022-05-15 16:13:39 -0400

    finished dad Jokes lesson

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,8 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +13,34 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  loader.classList.remove('hidden');
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  loader.classList.add('hidden');
+  return data;
+}
+
+function randomItemFromArray(array, not) {
+  const item = array[Math.floor(Math.random() * array.length)];
+  if (item === not) {
+    return randomItemFromArray(array, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButtonSpan.textContent = randomItemFromArray(
+    buttonText,
+    jokeButtonSpan.textContent
+  );
+}
+
+jokeButton.addEventListener('click', handleClick);
