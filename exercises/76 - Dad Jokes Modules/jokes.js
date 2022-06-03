f4db508a155440be6e61b71b592ab0b3b360a2b5
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2022-06-03 17:25:10 -0400

    reorganized folders to make sapce for npm modules lesson

--- exercises/76 - Dad Jokes Modules/jokes.js
+++ exercises/76 - Dad Jokes Modules/jokes.js
@@ -1,4 +1,4 @@
 import { handleClick } from './lib/handlers.js';
-import jokeButton from './lib/elements.js';
+import { jokeButton } from './lib/elements.js';
 
 jokeButton.addEventListener('click', handleClick);
